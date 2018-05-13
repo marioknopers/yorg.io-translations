f658ab220ae8b84af335935af026312e474fe7b1
Tobias Springer <tobias.springer1@googlemail.com>
2018-05-13 18:53:04 +0200

    Initial Commit

---
+++ translations/de.js
@@ -0,0 +1,261 @@
+export const TRANSLATIONS_DE = {
+
+    // Welcome screen
+
+    "welcome_introduction_header": "Einführung",
+    "welcome_introduction": "Willkommen auf yorg.io, ein Spiel über Versorgungsketten und Zombies! Überlege solange du kannst - Bist du besser als deine Freunde?",
+
+    "loading_game": "Laden ...",
+    "play_button": "SPIELEN",
+    "tutorial_button": "Tutorial",
+
+    "playername_input_placeholder": "Nickname",
+
+    "discord": "Discord",
+    "reddit": "Reddit",
+    "twitter": "Twitter",
+    "wiki": "Wiki",
+    "featured_youtuber": "Youtuber",
+
+    "thirdparty_hint_header": "Spiele direkt auf YORG.io",
+    "thirdparty_hint": "Du spielst auf einer externen Seite. Spiele direkt auf yorg.io für ein besseres Spielerlebnis. <b>Klicke hier</b> um zu YORG.io zu gelangen",
+
+    "adblock_hint_header": "Bitte deaktiviere deinen adblocker",
+    "adblock_hint": "YORG.io ist kostenlos, aber bitte deaktiviere deinen adblocker um uns zu unterstützen! <b>Klicke hier</b> um herauszufinden wie.",
+
+    "changelog": "Änderungshistorie",
+    "view_full_changelog": "Zeige alle Einträge",
+
+    "footer_licenses_partners": "Lizenzen & Partner",
+    "footer_more_io_games": "Mehr .io Games",
+    "footer_press_kit": "Presse",
+    "footer_imprint": "Impressum",
+    "cookie_hint": `Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.
+        <a href='https://website-tutor.com/datenschutz/' target='_blank'>Mehr erfahren</a>`,
+
+    "disable_adblock_title": "How to: adblock deaktivieren",
+    "disable_adblock_instructions": `
+        Visuelle Anleitung: <a href="http://cdn.epicbundle.com/image/article/adblock_how-to_disable_ad-blocker_turn-off.png" target="_blank">hier</a>.<br />
+        <br />
+        <b>AdBlock Plus / ABP</b><br />
+        1. Klicke auf das AdBlock Plus Erweiterungs Icon.<br />
+        2. Klicke auf "Enabled on this site" um AdBlock für die aktuelle Seite zu deaktivieren.<br />
+        <br />
+        <b>uBlock Origin</b><br />
+        1. Klicke auf das uBlock Erweiterungs Icon.<br />
+        2. Klicke auf den blauen "power" Button um AdBlock für die aktuelle Seite zu deaktivieren.<br />
+        <br />
+        <b>AdBlock</b>
+        <br />
+        1. Klicke auf das AdBlock Erweiterungs Icon.<br />
+        2. Klicke "Don‘t run on this domain" um AdBlock für die aktuelle Seite zu deaktivieren<br /><br />
+        Danke, dass du uns unterstützt!
+    `,
+
+    "report_bug_button": "Fehler oder Vorschlag?",
+    "report_bug_title": "Hast du einen Fehler gefunden, oder Vorschläge?",
+    "rebort_bug_inner": `
+        Falls du denkst einen Fehler gefunden zu haben, oder Vorschläge hast, gibt es mehrere Möglichkeiten:<br /><br />
+        <ul>
+            <li>Trete dem offiziellen <a href="<%= htmlWebpackPlugin.options.env.discordLink %>" target="_blank">Discord</a> bei</li>
+            <li>Besuche das <a href="https://www.reddit.com/r/yorgio/" target="_blank">YORG.io subreddit</a></li>
+            <li>Schreibe uns eine E-Mail an <a href="mailto:feedback@yorg.io">feedback@yorg.io</a></li>
+        </ul>
+        <br />
+        Wenn du einen Fehler meldest, bitte <b>hänge die Konsolenausgabe an</b>. Du kannst diese einsehen indem du
+        F12 drückst, und in den <i>Console</i> tab wechselst.
+    `,
+
+    "close_dialog": "Schließen",
+
+    // Restart dialog
+    "restart_game_confirm": "Bist du sicher dass du das Spiel neu starten willst? <b>Du verlierst deinen gesamten Fortschritt!</b>",
+    "restart_game_button": "Neu starten",
+    "restart_continue_playing": "Weiterspielen",
+
+    // Ingame
+
+    // Visualizers
+    "crystals_per_sec": "Kristalle pro Sekunde",
+
+    "cannon": "Kanone",
+    "arrow": "Pfeil",
+    "lightning": "Blitz",
+
+    "iron": "Eisen",
+    "wood": "Holz",
+    "uranium": "Uran",
+    "steel": "Stahl",
+    "trunks": "Stämme",
+    "power": "Strom",
+
+    // Game time
+
+    "pause_not_possible_during_night": "Du kannst während der Nacht nicht pausieren!",
+    "pause_not_possible_before_base": "Du kannst nicht pausieren bevor du deine Basis gebaut hast!",
+
+    "fastforward_disabled_while_boss": "Zeitraffer ist deaktiviert während der Boss noch lebt!",
+    "fastforward_not_possible_before_base": "Zeitraffer ist deaktiviert bevor du deine Basis gebaut hast!",
+
+    "pause_game": "Pausieren",
+    "pause_game_desc": "Pausiere das Spiel",
+
+    "regular_speed": "Normale Geschwindigkeit",
+    "regular_speed_desc": "Spiele das Spiel mit normaler Geschwindigkeit",
+
+    "fast_forward": "Zeitraffer",
+    "fast_forward_desc": "Spiele das Spiel mit erhöter Geschwindigkeit",
+
+    // Menu buttons
+
+    "keyboard_shortcuts": "Tastenkürzel",
+    "keyboard_shortcuts_desc": "Zeige den Tastenkürzel Dialog",
+
+    "restart_game": "Neu starten",
+    "restart_game_desc": "Hier kannst du das Spiel neu starten",
+
+    "toggle_fullscreen": "Vollbild an/aus",
+    "toggle_fullscreen_desc": "Hier kannst du Vollbild umschalten, funktioniert nicht überall!",
+
+    // Views
+
+    "defense_view": "Turm Radius Ansicht",
+    "defense_view_desc": "Zeigt den Radius aller Verteidigungstürme",
+
+    "transport_view": "Transport Netzwerk",
+    "transport_view_desc": "Zeigt die Wege der Resourcen. Fahre über ein Gebäude um die Wege zu sehen.",
+
+    "processor_usage_view": "Auslastung",
+    "processor_usage_view_desc": "Zeigt die aktuelle Auslastung deiner Fabriken. Grün steht für volle Auslastung, Orange oder Rot zeigt dass die Fabrik noch mehr produzieren könnte.",
+
+    // Buildings bar
+
+    "prefix_hotkey": "Kürzel: ",
+    "no_cost": "kostenlos",
+
+    // Building tooltip
+    "level": "Level",
+
+    "stat_health": "Leben",
+    "stat_damage": "Schaden",
+    "stat_throughput": "Produziert /s",
+    "stat_radius": "Radius",
+    "stat_outcome": "Anzahl",
+    "stat_transportSpeed": "Geschwindigkeit",
+    "stat_spread": "Verbreitung",
+    "stat_shootsPerSecond": "Feuerrate",
+    "stat_storage": "Speicher",
+    "stat_consumeAmount": "Verbrauch",
+
+    "can_not_sell_building": "Du kannst dieses Gebäude nicht verkaufen",
+
+    "sell_for": "Verkaufen",
+
+    "upgrade": "Verbessern",
+    "upgrade_all": "Alle Verbessern",
+
+    "max_out": "Maximieren",
+    "max_out_all": "Alle Maximieren",
+
+    "upgrade_base_to_upgrade_building": "Verbessere deine Basis um dieses Gebäude zu verbessern!",
+    "no_further_upgrades": "Keine weitere Verbesserung möglich!",
+
+    // Buildings
+    "building_wall": "Mauer",
+    "building_wall_desc": "Beschützt deine Basis vor Zombies indem es ihren Weg blockiert",
+
+    "building_arrowFactory": "Pfeil Fabrik",
+    "building_arrowFactory_desc": "Produziert Pfeile aus Stahl und Stämmen",
+
+    "building_arrowTower": "Bogenschütze",
+    "building_arrowTower_desc": "Schießt mit Pfeilen auf einzelne Zombies",
+
+    "building_cannon": "Kanone",
+    "building_cannon_desc": "Schießt Kanonenkugeln, die explodieren wenn sie auf Zombies treffen.",
+
+    "building_cannonballProducer": "Kanonenkugel Fabrik",
+    "building_cannonballProducer_desc": "Produziert Kanonengugeln aus 2 unverarbeitetem Eisen",
+
+    "building_goldMine": "Kristallmine",
+    "building_goldMine_desc": "Extrahiert Kristalle aus Kristallkugeln. Liefer sie an deine Basis!",
+    "error_place_next_to_crystal": "Neben einem Kristall platzieren",
+
+    "building_harvester": "Holzfäller",
+    "building_harvester_desc": "Produziert unverarbeitetes Holz aus Wäldern",
+    "error_place_next_to_tree": "Neben einem Wald platzieren",
+
+    "building_ironMine": "Eisenmine",
+    "building_ironMine_desc": "Extrahiert unverarbeitetes Eisen aus Eisenerz.",
+    "error_place_next_to_iron_ore": "Neben einem Eisenerz platzieren",
+
+    "building_lightningTower": "Blitzturm",
+    "building_lightningTower_desc": "Schießt Blitze auf Gegner, welche sich dann weiter ausbreiten.",
+
+    "building_nuclearStation": "Atomkraftwerk",
+    "building_nuclearStation_desc": "Benutzt Uran um Strom zu produzieren",
+
+    "building_playerBase": "Basis",
+    "building_playerBase_desc": "Deine Basis - Beschützte sie, du verlierst wenn sie zerstört wurde!",
+    "error_only_one_base": "Du kannst nur eine Basis haben",
+    "error_base_unreachable": "Basis ist unerreichbar",
+
+    "building_steelFactory": "Stahlfabrik",
+    "building_steelFactory_desc": "Produziert Stahl aus 2 Holz und 1 Eisen.",
+
+    "building_transporter": "Transporter",
+    "building_transporter_desc": "Transportiert Ressourcen in seinem Einflussradius. Verkettbar.",
+
+    "building_uraniumMine": "Uranmine",
+    "building_uraniumMine_desc": "Extrahiert Uran aus Uranerz",
+    "error_place_next_to_uranium_ore": "Neben einem uranerz platzieren",
+
+    "building_woodProcessor": "Sägewerk",
+    "building_woodProcessor_desc": "Produziert Stämme aus 2 Holz",
+
+    // Building requirements
+    "place_dependent_building_first": "Baue zuerst %1!",
+    "upgrade_base_to_level_first": "Verbessere deine Basis auf Level %1 zum freischalten!",
+    "upgrade_base_first": "Verbessere deine Basis zuerst!",
+    "can_not_afford_upgrade": "Verbesserung ist zu teuer",
+    "already_at_max_level": "Bereits maximal verbessert",
+    "not_in_map_bounds": "Nicht auf der Karte",
+    "can_not_afford": "Zu Teuer",
+    "space_already_occupied": "Platz ist schon belegt",
+    "tile_occupied_by_enemies": "Platz ist durch Gegner belegt",
+
+    // Zombie waves timers
+    "first_zombie_wave_in": "Erste Zombie Welle in",
+    "zombie_boss_in": "Zombie Boss in",
+
+    "minute_suffix": "m",
+    "seconds_suffix": "s",
+
+    // Leaderboard
+    "leaderboard_rank": "#",
+    "leaderboard_name": "Name",
+    "leaderboard_day": "Tag",
+
+    // Keyboard shortcuts dialog
+
+    "kb_section_building": "Gebäude",
+    "kb_section_upgrading": "Verbessern",
+    "kb_section_movement": "Bewegung",
+    "kb_section_misc": "Sonstiges",
+
+    "kb_place_multiple": "Mehrere Bauen",
+    "kb_cancel_placement": "Bauen abbrechen",
+    "kb_sell_building": "Verkaufen",
+
+    "kb_move": "Bewegen",
+    "kb_move_alternate": "Bewegen #2",
+    "kb_mouse_hint": "Tipp: Du kannst die Karte auch mit der Maus ziehen",
+
+    "zoom": "Vergrößerung",
+    "mouse_wheel": "Mausrad",
+
+    "key_shift": "Shift",
+    "key_esc": "Esc",
+    "key_alt": "Alt",
+    "key_space": "Leertaste"
+}
+
