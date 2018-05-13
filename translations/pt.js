7130209132b508a27d62b338a6c42b26779ac236
Tobias Springer <tobias.springer1@googlemail.com>
2018-05-13 19:02:07 +0200

    Add more stubs

---
+++ translations/pt.js
@@ -0,0 +1,263 @@
+// NOT TRANSLATED YET
+
+export const TRANSLATIONS_PT = {
+
+    // Welcome screen
+
+    "welcome_introduction_header": "Introduction",
+    "welcome_introduction": "Welcome to yorg.io, a game about supply chains and defending your game against zombies! Your goal is to survive as long as possible. Can you beat your friends?",
+
+    "loading_game": "Loading Game ...",
+    "play_button": "PLAY",
+    "tutorial_button": "Tutorial",
+
+    "playername_input_placeholder": "Nickname",
+
+    "discord": "Discord",
+    "reddit": "Reddit",
+    "twitter": "Twitter",
+    "wiki": "Wiki",
+    "featured_youtuber": "Featured YT",
+
+    "thirdparty_hint_header": "Consider playing directly on YORG.io",
+    "thirdparty_hint": "You are playing on a thirdparty site. Consider playing on YORG.io directly for an improved experience! <b>Click here</b> to visit YORG.io",
+
+    "adblock_hint_header": "Please disable your adblocker",
+    "adblock_hint": "YORG.io is free to play, but please consider disabling your adblocker to support us! <b>Click here</b> to find out how to disable it.",
+
+    "changelog": "Changelog",
+    "view_full_changelog": "View full changelog",
+
+    "footer_licenses_partners": "Licenses & Partners",
+    "footer_more_io_games": "More .io Games",
+    "footer_press_kit": "Press Kit",
+    "footer_imprint": "Imprint",
+    "cookie_hint": `This website uses cookies to ensure you get the best experience
+        on our website. <a href='https://cookiesandyou.com/' target='_blank'>Learn more</a>`,
+
+    "disable_adblock_title": "How to: Disabling adblock",
+    "disable_adblock_instructions": `
+        You can find a visual guide <a href="http://cdn.epicbundle.com/image/article/adblock_how-to_disable_ad-blocker_turn-off.png" target="_blank">here</a>.<br />
+        <br />
+        <b>AdBlock Plus / ABP</b><br />
+        1. Click on the AdBlock Plus extension icon.<br />
+        2. Click "Enabled on this site" to disable AdBlock for the current site.<br />
+        <br />
+        <b>uBlock Origin</b><br />
+        1. Click on the uBlock extension icon.<br />
+        2. Click the blue "power" button to disable AdBlock for the current site<br />
+        <br />
+        <b>AdBlock</b>
+        <br />
+        1. Click on the AdBlock extension icon.<br />
+        2. Click "Don‘t run on this domain" to disable AdBlock for the current site.<br /><br />
+        Thank you for supporting us!
+    `,
+
+    "report_bug_button": "Rebort Bug or Suggestion",
+    "report_bug_title": "Found a Bug or Suggestion?",
+    "rebort_bug_inner": `
+        If you think you found a bug, or you have ideas for improvements, there are several possibilities:<br /><br />
+        <ul>
+            <li>Join the official <a href="<%= htmlWebpackPlugin.options.env.discordLink %>" target="_blank">Discord</a></li>
+            <li>Visit the <a href="https://www.reddit.com/r/yorgio/" target="_blank">YORG.io subreddit</a></li>
+            <li>Write an email to <a href="mailto:feedback@yorg.io">feedback@yorg.io</a></li>
+        </ul>
+        <br />
+        When a reporting a bug, please <b>include the console output</b>. You can get it by pressing F12 and selecting the <i>Console</i> tab.
+    `,
+
+    "close_dialog": "Close",
+
+    // Restart dialog
+    "restart_game_confirm": "Are you sure you want to restart the game? This will make you <b>lose all of your progress!</b>",
+    "restart_game_button": "Restart",
+    "restart_continue_playing": "Continue Playing",
+
+    // Ingame
+
+    // Visualizers
+    "crystals_per_sec": "Crystals per sec",
+
+    "cannon": "Cannon",
+    "arrow": "Arrow",
+    "lightning": "Lightning",
+
+    "iron": "Iron",
+    "wood": "Wood",
+    "uranium": "Uranium",
+    "steel": "Steel",
+    "trunks": "Trunks",
+    "power": "Power",
+
+    // Game time
+
+    "pause_not_possible_during_night": "Pause is not possible during night!",
+    "pause_not_possible_before_base": "Pause is not available before your base was placed!",
+
+    "fastforward_disabled_while_boss": "Fast forward is disabled while the boss is alive!",
+    "fastforward_not_possible_before_base": "Fast forward is not available before your base was placed!",
+
+    "pause_game": "Pause Game",
+    "pause_game_desc": "This pauses the game",
+
+    "regular_speed": "Regular Speed",
+    "regular_speed_desc": "This plays the game at regular speed",
+
+    "fast_forward": "Fast Forward",
+    "fast_forward_desc": "This fast forwards the game",
+
+    // Menu buttons
+
+    "keyboard_shortcuts": "Keyboard Shortcuts",
+    "keyboard_shortcuts_desc": "Show the keyboard shortcut dialog",
+
+    "restart_game": "Restart Game",
+    "restart_game_desc": "Allows you to restart the game",
+
+    "toggle_fullscreen": "Toggle Fullscreen",
+    "toggle_fullscreen_desc": "Allows to toggle fullscreen, does not work on all pages!",
+
+    // Views
+
+    "defense_view": "Tower Range View",
+    "defense_view_desc": "Showing the range of all defensive towers",
+
+    "transport_view": "Transport Network",
+    "transport_view_desc": "Showing resource transport ways. Mouseover a building to see its transport ways.",
+
+    "processor_usage_view": "Utilization View",
+    "processor_usage_view_desc": "Showing the current utilization of all your factories. Green indicates that the production is at full capacity, yellow or red means that the production is not running at full speed.",
+
+    // Buildings bar
+
+    "prefix_hotkey": "Hotkey: ",
+    "no_cost": "no cost",
+
+    // Building tooltip
+    "level": "Level",
+
+    "stat_health": "Health",
+    "stat_damage": "Damage",
+    "stat_throughput": "Produce /s",
+    "stat_radius": "Radius",
+    "stat_outcome": "Amount",
+    "stat_transportSpeed": "Speed",
+    "stat_spread": "Spread",
+    "stat_shootsPerSecond": "Fire rate",
+    "stat_storage": "Storage",
+    "stat_consumeAmount": "Consumes",
+
+    "can_not_sell_building": "You can not sell this building",
+
+    "sell_for": "Sell for",
+
+    "upgrade": "Upgrade",
+    "upgrade_all": "Upgrade all",
+
+    "max_out": "Max out",
+    "max_out_all": "Max out all",
+
+    "upgrade_base_to_upgrade_building": "Upgrade your Base to upgrade this building!",
+    "no_further_upgrades": "No further upgrade possible!",
+
+    // Buildings
+    "building_wall": "Wall",
+    "building_wall_desc": "Protects your base from zombies by blocking their movement",
+
+    "building_arrowFactory": "Arrow Factory",
+    "building_arrowFactory_desc": "Produces arrows out of steel and tree trunks",
+
+    "building_arrowTower": "Arrow Tower",
+    "building_arrowTower_desc": "Single target tower which shoots arrows at zombies",
+
+    "building_cannon": "Cannon",
+    "building_cannon_desc": "Shoots cannonballs which explode when hitting zombies",
+
+    "building_cannonballProducer": "Cannon Ball Factory",
+    "building_cannonballProducer_desc": "Produces cannon balls out of 2 unprocessed iron",
+
+    "building_goldMine": "Crystal Mine",
+    "building_goldMine_desc": "Extracts crystals from crystal orbs. Deliver them to your base",
+    "error_place_next_to_crystal": "Place next to a crystal",
+
+    "building_harvester": "Harvester",
+    "building_harvester_desc": "Harvests forests and produces unprocessed wood",
+    "error_place_next_to_tree": "Place next to a tree",
+
+    "building_ironMine": "Iron Mine",
+    "building_ironMine_desc": "Extracts unprocessed iron from iron ores",
+    "error_place_next_to_iron_ore": "Place next to an iron ore",
+
+    "building_lightningTower": "Lightning Tower",
+    "building_lightningTower_desc": "Shoots lightning bolts on enemies which then spread to other enemies",
+
+    "building_nuclearStation": "Nuclear Reactor",
+    "building_nuclearStation_desc": "Uses uranium to produce nuclear power",
+
+    "building_playerBase": "Base",
+    "building_playerBase_desc": "Your base - Protect it, once destroyed the game is lost",
+    "error_only_one_base": "You may only place one Base",
+    "error_base_unreachable": "Base is unreachable",
+
+    "building_steelFactory": "Steel Factory",
+    "building_steelFactory_desc": "Produces steel out of 2 unprocessed wood and 1 iron",
+
+    "building_transporter": "Transporter",
+    "building_transporter_desc": "Transports resources within its range of influence. Chainable.",
+
+    "building_uraniumMine": "Uranium Mine",
+    "building_uraniumMine_desc": "Extracts uranium from uranium ores",
+    "error_place_next_to_uranium_ore": "Place next to a uranium ore",
+
+    "building_woodProcessor": "Sawmill",
+    "building_woodProcessor_desc": "Produces tree trunks out of 2 unprocessed wood",
+
+    // Building requirements
+    "place_dependent_building_first": "Place a %1 first!",
+    "upgrade_base_to_level_first": "Upgrade your base to level %1 to unlock!",
+    "upgrade_base_first": "Upgrade your base first!",
+    "can_not_afford_upgrade": "Can not afford upgrade",
+    "already_at_max_level": "Already at maximum level",
+    "not_in_map_bounds": "Not in map bounds",
+    "can_not_afford": "Can't afford",
+    "space_already_occupied": "Space is already occupied",
+    "tile_occupied_by_enemies": "Tile is occupied by enemies!",
+
+    // Zombie waves timers
+    "first_zombie_wave_in": "First zombie wave in",
+    "zombie_boss_in": "Zombie Boss in",
+
+    "minute_suffix": "m",
+    "seconds_suffix": "s",
+
+    // Leaderboard
+    "leaderboard_rank": "#",
+    "leaderboard_name": "Name",
+    "leaderboard_day": "Day",
+
+    // Keyboard shortcuts dialog
+
+    "kb_section_building": "Building",
+    "kb_section_upgrading": "Upgrading",
+    "kb_section_movement": "Movement",
+    "kb_section_misc": "Misc",
+
+    "kb_place_multiple": "Place Multiple",
+    "kb_cancel_placement": "Cancel Placement",
+    "kb_sell_building": "Sell Building",
+
+    "kb_move": "Move",
+    "kb_move_alternate": "Move #2",
+    "kb_mouse_hint": "Hint: You can also move by dragging the map with your mouse",
+
+    "zoom": "Zoom",
+    "mouse_wheel": "Mouse Wheel",
+
+    "key_shift": "Shift",
+    "key_esc": "Esc",
+    "key_alt": "Alt",
+    "key_space": "Space"
+
+
+}
